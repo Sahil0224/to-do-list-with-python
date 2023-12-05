db23076d55f706d8f8e4c6a11537793209a96544
Patel <sahil.patel@fiserv.com>
2023-12-05 13:02:02 -0500

    Changes.

--- first-flask-app/static/darkmode.js
+++
@@ -1,5 +0,0 @@
-let dark_mode = function(){
-    var element = document.body;
-    element.classList.toggle("dark_mode");
-}
-dark_mode_button.addEventListener('click', dark_mode);
--- first-flask-app/static/script.js
+++ first-flask-app/static/script.js
@@ -92,8 +92,3 @@ let clear = function () {
  
 clearButton.addEventListener('click', clear);
 
-// let dark_mode = function(){
-//     var element = document.body;
-//     element.classList.toggle("dark_mode");
-// }
-// dark_mode_button.addEventListener('click', dark_mode);
